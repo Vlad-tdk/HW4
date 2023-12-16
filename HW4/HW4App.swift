e32524cfc0d7ad01395cfd0e299ba725d4d1fece
Vladimir <xalif80@gmail.com>
2023-12-16 17:07:25 +0100

    Initial Commit

---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by Vlad on 16.12.23..
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HW4/SliderViewUIkit.swift
@@ -0,0 +1,8 @@
+//
+//  SliderViewUIkit.swift
+//  HW4
+//
+//  Created by Vlad on 16.12.23..
+//
+
+import Foundation
